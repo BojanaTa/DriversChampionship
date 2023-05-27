7c9e13c9dd499ed9b153d6f603041e603c417754
kristijana87 <kristijana87@gmail.com>
2023-05-27 21:34:00 +0200

    Add changes in TeamsDetails

--- src/components/TeamsDetails.js
+++ src/components/TeamsDetails.js
@@ -1,21 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Flag from "react-flagkit";
 
 const TeamsDetails = () => {
   const { id } = useParams();
   const [races, setRaces] = useState([]);
   const [raceDetails, setRaceDetails] = useState([]);
+  const [flagsDetails, setFlagsDetails] = useState([])
   const [isLoading, setIsLoading] = useState(true);
 
   const getRaceResults = async (id) => {
     const url = `https://ergast.com/api/f1/2013/constructors/${id}/results.json`;
+    const urlFlags = "https://raw.githubusercontent.com/Dinuks/country-nationality-list/master/countries.json";
     try {
       const response = await axios.get(url);
+      const responseFlags = await axios.get(urlFlags);
       const raceData = response.data.MRData.RaceTable.Races;
       console.log("Race Data:", raceData);
       setRaces(raceData);
       setRaceDetails(response.data.MRData.RaceTable.Races[0].Results);
+      setFlagsDetails(responseFlags.data);
       setIsLoading(false);
     } catch (error) {
       console.error(`Error retrieving race results:`, error);
@@ -50,6 +57,28 @@ const TeamsDetails = () => {
     }
   }
 
+  const getFlag = (raceCountry) => {
+    let country = "";
+
+    if (raceCountry === "UK") {
+      country = "United Kingdom of Great Britain and Northern Ireland";
+    } else if (raceCountry === "Korea") {
+      country = "Korea (Republic of)";
+    } else if (raceCountry === "UAE") {
+      country = "Saudi Arabia";
+    } else if (raceCountry === "USA") {
+      country = "United States of America";
+    } else {
+      country = raceCountry;
+    }
+
+    const flag = flagsDetails.find(item => item.en_short_name === country);
+
+    return flag.alpha_2_code;
+  }
+
+
+
 
   useEffect(() => {
     getRaceResults(id);
@@ -64,9 +93,9 @@ const TeamsDetails = () => {
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-        <div>
-          <h2>Race Results for </h2>
-          <table>
+        <div className="container">
+          <div className="header">Race Results for </div>
+          <table className="custom-table">
             <thead>
               <tr>
                 <th>Round</th>
@@ -81,7 +110,9 @@ const TeamsDetails = () => {
               {races.map((race, index) => (
                 <tr key={index}>
                   <td>{race.round}</td>
-                  <td>{race.raceName}</td>
+                  <td>
+                    <Flag country={getFlag(race.Circuit.Location.country)} />
+                    {race.raceName}</td>
                   {race.Results.map((result, index) => (
                     <td key={index} style={{ backgroundColor: getColor(result.position) }}>{result.position}</td>
                   ))}
