bb02c5e133f4250b776df07366dbe8ee626316dc
kristijana87 <kristijana87@gmail.com>
2023-05-29 14:06:25 +0200

    Add changes to Teams and TeamsDetails

--- src/components/TeamsDetails.js
+++ src/components/TeamsDetails.js
@@ -1,29 +1,36 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { Link, useLocation } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 import Flag from "react-flagkit";
+import { FadeLoader } from "react-spinners";
 
 const TeamsDetails = () => {
   const { id } = useParams();
+  const [teams, setTeams] = useState([]);
   const [races, setRaces] = useState([]);
   const [raceDetails, setRaceDetails] = useState([]);
-  const [flagsDetails, setFlagsDetails] = useState([])
+  const [flagsDetails, setFlagsDetails] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
 
   const getRaceResults = async (id) => {
+    const urlTeams = `http://ergast.com/api/f1/2013/constructors/${id}/constructorStandings.json`;
     const url = `https://ergast.com/api/f1/2013/constructors/${id}/results.json`;
     const urlFlags = "https://raw.githubusercontent.com/Dinuks/country-nationality-list/master/countries.json";
     try {
+      const responseTeams = await axios.get(urlTeams);
       const response = await axios.get(url);
       const responseFlags = await axios.get(urlFlags);
+      const teams = responseTeams.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+      console.log("Teams", teams);
+      setTeams(teams);
       const raceData = response.data.MRData.RaceTable.Races;
       console.log("Race Data:", raceData);
       setRaces(raceData);
       setRaceDetails(response.data.MRData.RaceTable.Races[0].Results);
       setFlagsDetails(responseFlags.data);
       setIsLoading(false);
+      setLoading(false);
     } catch (error) {
       console.error(`Error retrieving race results:`, error);
       setIsLoading(false);
@@ -34,7 +41,6 @@ const TeamsDetails = () => {
     console.log(position);
     switch (position) {
       case "1":
-        // console.log("yellow");
         return "gold";
       case "2":
         return "lightgray";
@@ -55,7 +61,7 @@ const TeamsDetails = () => {
       default:
         return "lavender";
     }
-  }
+  };
 
   const getFlag = (raceCountry) => {
     let country = "";
@@ -72,55 +78,136 @@ const TeamsDetails = () => {
       country = raceCountry;
     }
 
-    const flag = flagsDetails.find(item => item.en_short_name === country);
+    const flag = flagsDetails.find((item) => item.en_short_name === country);
 
     return flag.alpha_2_code;
-  }
+  };
+
+  const getFlag2 = (driverNationality) => {
+    
+    let nationality = "";
+
+    if (driverNationality === "British") {
+        nationality = "British, UK";
+    } else if (driverNationality === "Dutch") {
+        nationality = "Dutch, Netherlandic";
+    } else {
+        nationality = driverNationality;
+    }
+
+    const flag = flagsDetails.find(item => item.nationality === nationality);
+    
+    return flag.alpha_2_code;
+}
 
 
 
 
   useEffect(() => {
     getRaceResults(id);
-  }, [id]);
+  }, []);
+
+  if (loading) {
+    return (
+        <FadeLoader size={75} color="red" />
+    );
+  }
 
   console.log("Race Details:", raceDetails);
   console.log("isLoading:", isLoading);
-
 
   return (
     <div>
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-        <div className="container">
-          <div className="header">Race Results for </div>
-          <table className="custom-table">
-            <thead>
-              <tr>
-                <th>Round</th>
-                <th>Grand Prix</th>
-                {raceDetails.map((result, index) => (
-                  <th key={index}>{result.Driver.familyName}</th>
-                ))}
-                <th>Points</th>
-              </tr>
-            </thead>
-            <tbody>
-              {races.map((race, index) => (
-                <tr key={index}>
-                  <td>{race.round}</td>
+        <div className="wrap">
+          <div className="driver-details">
+            <table className="driver">
+              <thead>
+                <tr>
+                <td><img src={`/images/${raceDetails[0].Constructor.constructorId}.png`} alt="Teams Logo" /></td>  
                   <td>
-                    <Flag country={getFlag(race.Circuit.Location.country)} />
-                    {race.raceName}</td>
-                  {race.Results.map((result, index) => (
-                    <td key={index} style={{ backgroundColor: getColor(result.position) }}>{result.position}</td>
-                  ))}
-                  <td>{race.Results[0].points}</td>
+                   <Flag country={getFlag2(raceDetails[0].Constructor.nationality)}/>
+                    <p>{raceDetails[0].Constructor.name}</p>
+                  </td>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Country: </td>
+                  <td>{raceDetails[0].Driver?.nationality}</td>
+                </tr>
+                <tr>
+                  <td>Position: </td>
+                  <td>{raceDetails[0].position}</td>
+                </tr>
+                <tr>
+                  <td>Points: </td>
+                  <td>{raceDetails[0].points}</td>
+                </tr>
+                <tr>
+                  <td>History: </td>
+                  <td>
+                    {teams.length > 0 && (
+                      <Link
+                        to={teams[0].Constructor.url}
+                        target="_blank"
+                      >
+                        <img
+                          src="/images/link-white.png"
+                          alt="Teams Logo"
+                          className="link-btn"
+                        />
+                      </Link>
+                    )}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="container">
+            <div className="header">Race Results for</div>
+            <table className="custom-table">
+              <thead>
+                <tr>
+                  <th>Round</th>
+                  <th>Grand Prix</th>
+                  {raceDetails.map((result, index) => (
+                    <th key={index}>{result.Driver.familyName}</th>
+                  ))}
+                  <th>Points</th>
+                </tr>
+              </thead>
+              <tbody>
+                {races.map((race, index) => (
+                  <tr key={index}>
+                    <td>{race?.round}</td>
+                    <td>
+                      <Flag
+                        country={getFlag(race.Circuit.Location.country)}
+                      />
+                      {race.raceName}
+                    </td>
+                    {race.Results.map((result, index) => (
+                      <td
+                        key={index}
+                        style={{
+                          backgroundColor: getColor(result.position),
+                        }}
+                      >
+                        {result.position}
+                      </td>
+                    ))}
+                    <td>
+                      {parseInt(race.Results[0].points) +
+                        parseInt(race.Results[1].points)}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       )}
     </div>
@@ -128,6 +215,5 @@ const TeamsDetails = () => {
 };
 
 export default TeamsDetails;
-
 
 
