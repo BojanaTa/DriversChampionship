8bf6add8d452b1c8869478f6e3db633743fe426a
MarinaGazic <marina.gazic@yahoo.com>
2023-06-01 08:22:09 +0200

    Add RacesDetails

--- src/components/RacesDetails.js
+++ src/components/RacesDetails.js
@@ -44,141 +44,155 @@ const RacesDetails = () => {
         }
     }
 
-        const getColor = (position) => {
-            switch (position) {
-                case "1":
-                    return "gold";
-                case "2":
-                    return "lightgray";
-                case "3":
-                    return "lightsalmon";
-                case "4":
-                    return "lightgreen";
-                case "5":
-                    return "lightblue";
-                case "6":
-                case "7":
-                case "8":
-                case "9":
-                case "10":
-                case "11":
-                case "12":
-                    return "palegreen";
-                default:
-                    return "lavender";
-            }
-        };
-
-        useEffect(() => {
-            getResults();
-            //eslint-disable-next-line
-
-        }, []);
-
-        if (loading) {
-            return (
-                <Loader />
-            );
+    const getColor = (points) => {
+        switch (points) {
+            case "1":
+                return "gold";
+            case "2":
+                return "lightgray";
+            case "3":
+                return "lightsalmon";
+            case "4":
+                return "lightgreen";
+            case "5":
+                return "lightblue";
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+            case "10":
+            case "11":
+            case "12":
+                return "palegreen";
+            default:
+                return "lavender";
         }
+    };
 
+    const getBestTime = (result) => {
+        if (result.Q3) {
+            return result.Q3;
+        } else if (result.Q2) {
+            return result.Q2;
+        } else if (result.Q1) {
+            return result.Q1;
+        } else {
+            return "";
+        }
+    };
 
+    useEffect(() => {
+        getResults();
+        //eslint-disable-next-line
+
+    }, []);
+
+    if (loading) {
         return (
-            <div className="wrap">
-                <div className="driver-details">
-                    <table className="driver">
-                        <thead>
-                            <tr>
-                                <td>
-                                <Flag country={getFlagByCountry(qualifiersResults.Circuit.Location.country, dataContext.flagsDetails)} />
-
-                                    <p>{qualifiersResults.raceName}</p>
-                                </td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Country: </td>
-                                <td>{qualifiersResults.Circuit.Location.country}</td>
-                            </tr>
-                            <tr>
-                                <td>Location: </td>
-                                <td>{qualifiersResults.Circuit.Location.locality}</td>
-                            </tr>
-                            <tr>
-                                <td>Date: </td>
-                                <td>{qualifiersResults.date}</td>
-                            </tr>
-                            <tr>
-                                <td>Full Report: </td>
-                                <td><Link to={qualifiersResults.url}target="_blank"><img src="/images/link-white.png" alt="Drivers Logo" className="link-btn" /></Link>
-                                </td>
-                            </tr>
-
-                        </tbody>
-                    </table>
-                </div>
-                <div className="container">
-                    <div className="header">Qualifying Results</div>
-
-                    <table className="custom-table">
-                        <thead>
-                            <tr>
-                                <td>Pos</td>
-                                <td>Driver</td>
-                                <td>Team</td>
-                                <td>Best Time</td>
-
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {qualifiersResults.QualifyingResults.map((result, i) =>
-                                <tr key={i}>
-                                    <td>{result.position}</td>
-                                   <td> 
-                                    <Flag country={getFlagByNationality(result.Driver.nationality, dataContext.flagsDetails)} />
-                                      {result.Driver.familyName}
-                                    </td>
-                                    <td>{result.Constructor.name}</td>
-                                    <td></td>
-
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
-                    <div className="footer"></div>
-                </div>
-
-                <div className="container">
-                    <div className="header">Race Results</div>
-                    <table className="custom-table">
-                        <thead>
-                            <tr>
-                                <th>Pos</th>
-                                <th>Driver</th>
-                                <th>Team</th>
-                                <th>Result</th>
-                                <th>Points</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {results.map((result, i) =>
-                                <tr key={i}>
-                                    <td>{result.position}</td>
-                                    <td>
-                                        {result.Driver.familyName}
-                                    </td>
-                                    <td>{result.Constructor.name}</td>
-                                    <td>{result.Time !== undefined ? result.Time.time : ""}</td>
-                                    <td>{result.points}</td>
-                                </tr>
-                            )}
-                        </tbody>
-
-                    </table>
-                    <div className="footer"></div>
-                </div>
-            </div>
+            <Loader />
         );
     }
 
-    export default RacesDetails;
+
+    return (
+        <div className="wrap">
+            <div className="driver-details">
+                <table className="driver">
+                    <thead>
+                        <tr>
+                            <td>
+                                <Flag country={getFlagByCountry(qualifiersResults.Circuit.Location.country, dataContext.flagsDetails)} />
+
+                                <p>{qualifiersResults.raceName}</p>
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Country: </td>
+                            <td>{qualifiersResults.Circuit.Location.country}</td>
+                        </tr>
+                        <tr>
+                            <td>Location: </td>
+                            <td>{qualifiersResults.Circuit.Location.locality}</td>
+                        </tr>
+                        <tr>
+                            <td>Date: </td>
+                            <td>{qualifiersResults.date}</td>
+                        </tr>
+                        <tr>
+                            <td>Full Report: </td>
+                            <td><Link to={qualifiersResults.url} target="_blank"><img src="/images/link-white.png" alt="Drivers Logo" className="link-btn" /></Link>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+            <div className="container">
+                <div className="header">Qualifying Results</div>
+
+                <table className="custom-table">
+                    <thead>
+                        <tr>
+                            <td>Pos</td>
+                            <td>Driver</td>
+                            <td>Team</td>
+                            <td>Best Time</td>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {qualifiersResults.QualifyingResults.map((result, i) =>
+                            <tr key={i}>
+                                <td>{result.position}</td>
+                                <td>
+                                    <Flag country={getFlagByNationality(result.Driver.nationality, dataContext.flagsDetails)} />
+                                    {result.Driver.familyName}
+                                </td>
+                                <td>{result.Constructor.name}</td>
+                                <td>{getBestTime(result)}</td>
+
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+                <div className="footer"></div>
+            </div>
+
+            <div className="container">
+                <div className="header">Race Results</div>
+                <table className="custom-table">
+                    <thead>
+                        <tr>
+                            <th>Pos</th>
+                            <th>Driver</th>
+                            <th>Team</th>
+                            <th>Result</th>
+                            <th>Points</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {results.map((result, i) =>
+                            <tr key={i}>
+                                <td>{result.position}</td>
+                                <td>
+                                    <Flag country={getFlagByNationality(result.Driver.nationality, dataContext.flagsDetails)} />
+                                    {result.Driver.familyName}
+                                </td>
+                                <td>{result.Constructor.name}</td>
+                                <td>{result.Time !== undefined ? result.Time.time : ""}</td>
+                                <td style={{backgroundColor: getColor(result.points)}}>
+                                        {result.points}
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+                <div className="footer"></div>
+            </div>
+        </div>
+    );
+}
+
+export default RacesDetails;
